60abbfc8424f6ece622ab664ee92cbd0968a5180
Harsh Singh <104265750+harshsingh2985@users.noreply.github.com>
2022-07-04 11:07:18 +0530

    update:if or else statement in the server

---
+++ app.js
@@ -0,0 +1,28 @@
+// creating own server
+const http=require("http")
+
+const PORT=4000;
+
+const hostname="localhost";
+
+const server=http.createServer((req,res)=>{
+    // res.end("working on something big")
+    // res.end("<h1>hello world welcomes you </h1>")
+    
+    if(req.url==="/about"){
+        res.end("<h1>ABOUT-PAGE</h1>")
+    }
+    if(req.url==="/contact"){
+        res.end("<h1>Contact-Page</h1")
+    }
+    if(req.url==="/services"){
+        res.end("<h1>Servies-page</h1>")
+    }
+
+
+})
+
+server.listen(PORT,hostname,()=>{
+    console.log("server works on http://localhost:4000)")
+
+})
